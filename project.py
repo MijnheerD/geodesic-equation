8af960556e6b395b8fdc255d124510be632aa330
Mitja <34456399+MijnheerD@users.noreply.github.com>
2019-11-09 19:59:41 +0100

    Implemented differentiation of the metric to the coordinate mu

--- project.py
+++ project.py
@@ -1,1 +1,23 @@
 # Let's go!
+
+from sympy import *
+import numpy as np
+
+t = Symbol('t')
+x = Symbol('x')
+y = Symbol('y')
+z = Symbol('z')
+
+g = np.array([[t,0,0,0],[0,x,0,0],[0,0,y,0],[0,0,0,z]])
+
+def diffMetric(g,mu):
+    shape = g.shape
+    d = np.full(shape, 0)
+    r = len(g)
+    c = len(g[0])
+    for row in range(r):
+        for column in range(c):
+            d[row, column] = diff(g[row,column],mu)
+    return d
+
+print(diffMetric(g,y))
