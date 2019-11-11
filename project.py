6e30297f614d34dba60459a4c06c6102621303a2
Mitja <34456399+MijnheerD@users.noreply.github.com>
2019-11-11 15:47:17 +0100

    Made spacetime class and started on Christoffel symbols implementation

--- project.py
+++ project.py
@@ -8,7 +8,7 @@ x = Symbol('x')
 y = Symbol('y')
 z = Symbol('z')
 
-g = np.array([[t,0,0,0],[0,x,0,0],[0,0,y,0],[0,0,0,z],[t,0,t,0]])
+g = np.array([[t,0,0,0],[0,x,0,0],[0,0,y,0],[0,0,0,z]])
 
 def diffMetric(g,mu):
     shape = g.shape
@@ -20,4 +20,20 @@ def diffMetric(g,mu):
             d[row, column] = diff(g[row,column],mu)
     return d
 
-print(diffMetric(g,t))
+#Should make a class "Spacetime" with property g
+
+#No need for diffMetric?
+
+class Spacetime:
+    def __init__(self, g):
+        self.metric = g
+        self.symbols = [t, x, y, z]
+
+    def chrisSymbol(self, up, down1, down2):
+        t1 = sum([g[up, m] * diff(g[m, down1], down2) for m in self.symbols])
+        t2 = sum([g[up, m] * diff(g[m, down2], down1) for m in self.symbols])
+        t3 = sum([g[up, m] * diff(g[down1, down2], m) for m in self.symbols])
+        return 0.5*(t1+t2-t3)
+
+ST = Spacetime(g)
+print(ST.chrisSymbol(0,1,2))
