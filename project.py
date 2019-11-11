4f59e6587d046461977874e8f21ff5fa11f6303f
Mitja <34456399+MijnheerD@users.noreply.github.com>
2019-11-11 21:32:12 +0100

    Finished up Christoffel symbols, put everything into a class Spacetime, redefined diffMetric to better suit our purpose

--- project.py
+++ project.py
@@ -3,6 +3,31 @@
 from sympy import *
 import numpy as np
 
+
+#Should make a class "Spacetime" with property g
+
+class Spacetime:
+    def __init__(self, g):
+        self.metric = g
+        self.__symbols = [t, x, y, z]
+
+    def diffFullMetric(self, mu):  # Overkill for Christoffel symbols, might be useful later on
+        shape = self.metric.shape
+        d = np.full(shape, 0)
+        r = len(g)
+        c = len(g[0])
+        for row in range(r):
+            for column in range(c):
+                d[row, column] = diff(g[row, column], self.__symbols[mu])
+        return d
+
+    def diffMetric(self, pos, mu): #Mu given as number
+        return diff(g[pos[0], pos[1]], self.__symbols[mu])
+
+    def chrisSymbol(self, up, down1, down2):
+        return 0.5*sum([g[up, m] * self.diffMetric([m, down1], down2)+g[up, m] * self.diffMetric([m, down2], down1)-g[up, m] * self.diffMetric([down1, down2], m) for m in range(4)])
+
+
 t = Symbol('t')
 x = Symbol('x')
 y = Symbol('y')
@@ -10,30 +35,5 @@ z = Symbol('z')
 
 g = np.array([[t,0,0,0],[0,x,0,0],[0,0,y,0],[0,0,0,z]])
 
-def diffMetric(g,mu):
-    shape = g.shape
-    d = np.full(shape, 0)
-    r = len(g)
-    c = len(g[0])
-    for row in range(r):
-        for column in range(c):
-            d[row, column] = diff(g[row,column],mu)
-    return d
-
-#Should make a class "Spacetime" with property g
-
-#No need for diffMetric?
-
-class Spacetime:
-    def __init__(self, g):
-        self.metric = g
-        self.symbols = [t, x, y, z]
-
-    def chrisSymbol(self, up, down1, down2):
-        t1 = sum([g[up, m] * diff(g[m, down1], down2) for m in self.symbols])
-        t2 = sum([g[up, m] * diff(g[m, down2], down1) for m in self.symbols])
-        t3 = sum([g[up, m] * diff(g[down1, down2], m) for m in self.symbols])
-        return 0.5*(t1+t2-t3)
-
 ST = Spacetime(g)
-print(ST.chrisSymbol(0,1,2))
+print(ST.chrisSymbol(1,1,1))
