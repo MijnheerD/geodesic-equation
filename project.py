0a2a9f6d86fa71cf950b6bc0b513010b5ab26e83
Mitja <34456399+MijnheerD@users.noreply.github.com>
2019-11-23 16:36:58 +0100

    RK4 works now, changed the naming scheme to more accurately reflect process. Gives back everything in Symbols, will need a way to fill values in

--- project.py
+++ project.py
@@ -2,7 +2,7 @@
 
 from sympy import *
 import numpy as np
-from scipy.integrate import odeint
+from scipy.integrate import solve_ivp, odeint
 
 
 class Spacetime:
@@ -29,7 +29,7 @@ class Spacetime:
 #I added an s argument in order to use odeint for solving the difeq
 def geodesicEq(x, s, spacetime):
         if len(x)!=8:
-            raise TypeError("Input vector must have length 8")
+            raise TypeError("Input vector must have length 8, not "+str(len(x)))
         if type(spacetime) != Spacetime:
             raise TypeError("Input spacetime must be of type Spacetime")
         u = [0]*8
@@ -51,29 +51,30 @@ def solveGE(geodesicEq,xinit,ds,s0,s1,spacetime):
     return sol
 #Let's try with RK4=> still an error but now it says "
 #TypeError: can't multiply sequence by non-int of type 'float'
-#and the problem lays in k1 = h * f(yn,tn,spacetime) 
+#and the problem lays in k1 = h * f(yn,tn,spacetime)
 
-def RK4(yn, f, tn, h,spacetime):
-        k1 = h * f(yn,tn,spacetime) 
-        k2 = h * f(yn + 0.5 * k1,tn + 0.5 * h,spacetime) 
-        k3 = h * f(yn + 0.5 * k2,tn + 0.5 * h,spacetime) 
-        k4 = h * f(yn + k3, tn + h,spacetime)
-        return yn + (1.0 / 6.0)*(k1 + 2 * k2 + 2 * k3 + k4) 
+def list_mul(lst, f):
+    return [f * elem for elem in lst]
 
-def solveGE2(geodesicEq,xinit,ds,s0,s1,spacetime,h):
+def list_sum(lst1, lst2):
+    return [lst1[i] + lst2[i] for i in range(len(lst1))]
+
+def rkStep(yn, f, tn, h,spacetime):
+        k1 = [h * elem for elem in f(yn,tn,spacetime)]
+        k2 = [h * elem for elem in f(list_sum(yn, list_mul(k1, 0.5)), tn + 0.5 * h, spacetime)]
+        k3 = [h * elem for elem in f(list_sum(yn, list_mul(k1, 0.5)), tn + 0.5 * h, spacetime)]
+        k4 = [h * elem for elem in f(list_sum(yn, k3), tn + h, spacetime)]
+        return list_sum(yn, [(1.0 / 6.0) * elem for elem in (k1 + 2 * k2 + 2 * k3 + k4)])
+
+def RK4(geodesicEq,xinit,ds,s0,s1,spacetime,h):
     s=np.arange(s0,s1+ds,ds)
-    solution=RK4(xinit,geodesicEq,s,h,spacetime)
     sol=[]
-    for i in solution:
-        sol.append([i[0:4]])
+    x = xinit
+    for t in s:
+        x = rkStep(x,geodesicEq,t,h,spacetime)
+        sol.append(x[0:4])
     return sol
 
-s0=1
-s1=2
-ds=0.01
-s = np.arange(s0,s1+ds,ds)
-x = [1,1,2,3,3,4,3,5]
-u=solveGE2(geodesicEq,x,ds,s0,s1,ST2,0.5)
 
 
 #Examples of Metrics
@@ -82,12 +83,31 @@ x = Symbol('x')
 y = Symbol('y')
 z = Symbol('z')
 
+print(0.5*t)
+
 g = np.array([[t,0,0,0],[0,x,0,0],[0,0,y,0],[0,0,0,z]])
 ST = Spacetime(g)
 
 g_schwartz=np.array([[-(1-1/x),0,0,0],[0,1/(1-1/x),0,0],[0,0,x**2,0],[0,0,0,x**2*np.sin(3)]])
 ST2= Spacetime(g_schwartz)
 
-#Test GeodesicEq
+
+#Testing
+s0 = 1
+s1 = 5
+ds = 1
+s = np.arange(s0,s1+ds,ds)
 x = [1,1,2,3,3,4,3,5]
-print(geodesicEq(x,ST2))
+
+#geodesicEq
+#print(geodesicEq(x,2,ST2))
+
+#solver
+u=RK4(geodesicEq,x,ds,s0,s1,ST2,0.5)
+print(u)
+
+#sol = []
+#solution = solve_ivp(lambda t, y: geodesicEq(y,t,ST), (s0,s1), x,args=(ST,))
+#for i in solution:
+#    sol.append([i[0:4]])
+#print(sol)
