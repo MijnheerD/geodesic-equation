f509d5e8646d7ffd5582c585cbbb98622180ec41
Mitja <34456399+MijnheerD@users.noreply.github.com>
2019-12-03 18:45:55 +0100

    Put in the read/write functions. VPython installation is a bit messy though.

--- animation.py
+++ animation.py
@@ -1,45 +1,51 @@
 from project import *
-from vpython impotrt *
+from vpython import *
 from scipy.constants import G
 
 
-t = Symbol('t')
-x = Symbol('x')
-y = Symbol('y')
-z = Symbol('z')
-
-g = np.array([[t,0,0,0],[0,x,0,0],[0,0,y,0],[0,0,0,z]])
-ST = Spacetime(g)
-
-g_schwartz=np.array([[-(1-1/x),0,0,0],[0,1/(1-1/x),0,0],[0,0,x**2,0],[0,0,0,x**2*np.sin(3)]])
-ST2= Spacetime(g_schwartz)
-
-s0 = 1
-s1 = 5
-ds = 1
-u0 = [2,3,4,5,2,3,4,5]
-
-u = np.array(RK4(geodesicEq,u0,ds,s0,s1,ST2))
-u2 = np.array(solveGE(geodesicEq,u0,ds,s0,s1,ST2))
+def write_out(data, filename):
+    f = open(filename, "w")
+    #Assume data structure
+    t = data[0]
+    x = data[1]
+    y = data[2]
+    z = data[3]
+    for item in t:
+        f.write("%f " % item)
+    f.write("\n")
+    for item in x:
+        f.write("%f " % item)
+    f.write("\n")
+    for item in y:
+        f.write("%f " % item)
+    f.write("\n")
+    for item in z:
+        f.write("%f " % item)
 
 
 def animate_geodesic(file, M):
-    
-    #read data from file
-    
-    t = u2.t
-    r = u2.y[1]
-    theta = u2.y[2]
-    phi = u2.y[3]
-    
+    f = open(file, "r")
+    line = f.readline()
+    t = [float(elem) for elem in line.split()]
+    line = f.readline()
+    r = [float(elem) for elem in line.split()]
+    line = f.readline()
+    theta = [float(elem) for elem in line.split()]
+    line = f.readline()
+    phi = [float(elem) for elem in line.split()]
+    f.close()
+
     x = r * np.sin(theta) * np.cos(phi)
     y = r * np.sin(theta) * np.sin(phi)
     z = r * np.cos(theta)
     
     r_s = 2*G*M
     particle = sphere(pos = vector(x[0], y[0], z[0]), radius = 1, color = color.green)
-    black_hole = sphere(pos = vector(0,0,0), radius = r_s, color = color.red)
+    black_hole = sphere(pos = vector(0,0,0), radius = 1, color = color.red)
     
-    for i in range(len(x)):
-        rate(10)
+    for i in range(len(t)):
+        rate(1)
         particle.pos = vector(x[i], y[i], z[i])
+
+write_out([[0,1,2,3,4],[1,2,3,4,5],[1,2,3,4,5],[1,1,1,1,1]], "test.txt")
+animate_geodesic("test.txt", 10**11)
